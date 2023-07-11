a5d31bbfade1ee25682394f0662aba2eb6f38cfa
nguyenleanh <leanh1022002@gmail.com>
2023-07-12 00:03:37 +0700

    client side code

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        connection:
+        try
+        {
+            TcpClient client = new TcpClient("192.168.64.128", 1302);
+            string clientRequest = "Request 1";
+
+            int byteCount = Encoding.ASCII.GetByteCount(clientRequest+1);
+            byte[] sendData = new byte[byteCount];
+            sendData = Encoding.ASCII.GetBytes(clientRequest);
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(sendData, 0, sendData.Length);
+            Console.WriteLine("Sending request to server");
+
+            StreamReader sr = new StreamReader(stream);
+            string response = sr.ReadLine();
+            Console.WriteLine(response);
+
+            stream.Close();
+            client.Close();
+            Console.ReadKey();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            goto connection;
+        }
+    }
+}
