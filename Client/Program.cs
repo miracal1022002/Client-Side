90747e8cde4cefeb3b09ce99d02b6038bb066231
nguyenleanh <leanh1022002@gmail.com>
2023-07-13 11:36:10 +0700

    finished client code

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,21 +4,37 @@ using System.Text;
 using System.IO;
 class Program
 {
+    static string serverIP;
+    static Int32 serverPort;
     static void Main(string[] args)
     {
         connection:
         try
         {
-            TcpClient client = new TcpClient("192.168.64.128", 1302);
-            string clientRequest = "Request 1";
-
+            TcpClient client = new TcpClient();
+            serverIP = "192.168.64.128";
+            serverPort = 1302;
+            while (!client.Connected)
+            {
+                try
+                {
+                    client.Connect(serverIP, serverPort);
+                    Console.WriteLine("Connected to server successfully \nServer IP: " + serverIP + " --- Server Port: " + serverPort);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Failed to connect to server");
+                }
+            }
+            Console.WriteLine("Please enter your name: ");
+            string? clientRequest = Console.ReadLine();
             int byteCount = Encoding.ASCII.GetByteCount(clientRequest+1);
             byte[] sendData = new byte[byteCount];
             sendData = Encoding.ASCII.GetBytes(clientRequest);
 
             NetworkStream stream = client.GetStream();
             stream.Write(sendData, 0, sendData.Length);
-            Console.WriteLine("Sending request to server");
+            Console.WriteLine("Request sent to server");
 
             StreamReader sr = new StreamReader(stream);
             string response = sr.ReadLine();
